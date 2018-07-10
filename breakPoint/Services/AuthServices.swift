279d561037fa2b34ffab8688cf6838f2ac128af9
RamitSharma991 <RamitsMac@Ramits-MacBook-Pro.local>
2018-07-10 16:54:46 +0530

    login&AuthAdded

---
+++ breakPoint/Services/AuthServices.swift
@@ -0,0 +1,40 @@
+//
+//  AuthServices.swift
+//  breakPoint
+//
+//  Created by Ramit sharma on 10/07/18.
+//  Copyright © 2018 Ramit sharma. All rights reserved.
+//
+
+import Foundation
+import Firebase
+import FirebaseAuth
+
+
+class AuthService {
+    static let instance = AuthService()
+    
+    func registerUser(withEmail email: String, andPassword password: String, userCreationComplete: @escaping(_ status: Bool, _ error: Error?) -> ()) {
+        Auth.auth().createUser(withEmail: email, password: password) { (user, error) in
+            guard let user = user else {
+                userCreationComplete(false, error)
+                return
+            }
+            
+            let userData = ["provider": user.providerID, "email": user.email]
+            DataService.instance.createDBUser(uid: user.uid, userData: userData)
+            userCreationComplete(true, nil)
+        }
+    }
+    
+    func loginUser(withEmail email: String, andPassword password: String, loginComplete: @escaping(_ status: Bool, _ error: Error?) ->()) {
+        Auth.auth().signIn(withEmail: email, password: password) { (user, error) in
+            guard let user = user else {
+                loginComplete(false, error)
+                return
+            }
+            loginComplete(true, nil)
+        }
+        
+    }
+}
