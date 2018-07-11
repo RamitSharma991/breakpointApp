d58f31b4aa4782e5a831b51d8cde26fc35aa39ff
RamitSharma991 <RamitsMac@Ramits-MacBook-Pro.local>
2018-07-11 16:29:09 +0530

    addedMeVC

---
+++ breakPoint/Controller/MeVC.swift
@@ -0,0 +1,25 @@
+//
+//  MeVC.swift
+//  breakPoint
+//
+//  Created by Ramit sharma on 11/07/18.
+//  Copyright © 2018 Ramit sharma. All rights reserved.
+//
+
+import UIKit
+
+class MeVC: UIViewController {
+
+    @IBOutlet weak var imageOutlet: UIImageView!
+    @IBOutlet weak var emailOutlet: UILabel!
+    @IBOutlet weak var tableviewOutlet: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func signOutBtn(_ sender: Any) {
+    }
+    
+}
