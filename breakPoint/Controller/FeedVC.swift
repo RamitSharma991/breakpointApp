38f0e83d5b7f2d9fab871199b1ad5ac87bb49289
RamitSharma991 <RamitsMac@ramits-macbook-pro.local>
2018-07-17 10:18:35 +0530

    Written the message model and setup message feed from firebase

--- breakPoint/Controller/FeedVC.swift
+++ breakPoint/Controller/FeedVC.swift
@@ -10,11 +10,44 @@ import UIKit
 
 class FeedVC: UIViewController {
 
+    @IBOutlet weak var feedTableView: UITableView!
+    
+    var messageArray = [Message]()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
+        feedTableView.delegate = self
+        feedTableView.dataSource = self
     
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        DataService.instance.getAllFeedMessages { (returnMessagesArray) in
+            self.messageArray = returnMessagesArray
+            self.feedTableView.reloadData()
+        }
+  
+    }
+}
+
+extension FeedVC: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messageArray.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "feedCell") as? FeedCell
+            else { return UITableViewCell() }
+        
+        let image = UIImage(named: "defaultProfileImage")
+        let message = messageArray[indexPath.row]
+        
+        cell.configureCell(profileImage: image!, email: message.senderId, content: message.content)
+        return cell
+    }
 }
 
---
+++ breakPoint/Model/Message.swift
@@ -0,0 +1,28 @@
+//
+//  Message.swift
+//  breakPoint
+//
+//  Created by Ramit sharma on 16/07/18.
+//  Copyright © 2018 Ramit sharma. All rights reserved.
+//
+
+import Foundation
+
+class Message {
+    private var _content: String
+    private var _senderId: String
+    
+    var content: String {
+        return _content
+    }
+    
+    var senderId: String {
+        return _senderId
+    }
+    
+    init(content: String, senderId: String) {
+        self._content = content
+        self._senderId = senderId
+    }
+    
+}
